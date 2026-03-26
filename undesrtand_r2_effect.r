5654ed7bdc8dad6df8cfe5c23087627270f0bcf0
TheoCavinato <theo.cvnt@gmail.com>
2026-03-26 17:03:38 +0100

    there is something weird in this distirbution compared to the other simulations, UNDERSTAND WHAT IS MAKING THIS DIFFERENCE

--- undesrtand_r2_effect.r
+++ undesrtand_r2_effect.r
@@ -1,12 +1,15 @@
 library(ggplot2)
+library(mvtnorm)
 
 # Using a constant r2 instead of the actual r2 seem to work better
 # but why?
 # Here I try to address the problme directly thorugh simulations
 
-
+n = 1e3
 cat("Simulating data:\n")
-r2 = c(0.3529248, 0.3279128, 0.2788137, 0.2584552, 0.2166321, 0.2160767, 0.2069917, 0.1899249, 0.1854733, 0.1600537)
+r2 = c(0.924046, 0.593909, 0.306394, 0.578941, 0.740133, 0.786926, 0.43637, 0.332195, 0.77888, 0.100887)
+#r2 = c(0.3529248, 0.3279128, 0.2788137, 0.2584552, 0.2166321, 0.2160767, 0.2069917, 0.1899249, 0.1854733, 0.1600537)
+
 r=sqrt(r2)
 n_pheno = length(r2)
 
@@ -19,23 +22,53 @@ apply(E %*% diag(sqrt(1-r2)), 2, var) + r2 # should be close to one everywhere
 apply(X, 2, var)  # should be close to 1
 
 cat("Computing LLR:\n")
-diff_mat = X - PRS
-wrong_diff_mat = X - PRS[c(2:nrow(PRS), 1) ]
+compute_llr = function(X, PRS, r2, corr_mat_env) {
+	scaled_PRS = scale(PRS)
+	diff_mat = X - scaled_PRS %*% diag(sqrt(r2))
+	corr_mat_env = cor(E %*% diag(sqrt(1-r2))) 
 
-corr_mat_env = cor(E %*% diag(sqrt(1-r2))) 
+	p0 = dmvnorm(X,mean=rep(0,n_pheno),sigma=corr_mat_env,log = TRUE)
+	p1_h1 = dmvnorm(diff_mat ,mean=rep(0,n_pheno),sigma= diag(sqrt(1-r2)) %*% corr_mat_env %*% diag(sqrt(1-r2)),log = TRUE)
+	return(p1_h1 - p0)
+}
 
-p0 = dmvnorm(X,mean=rep(0,n_pheno),sigma=corr_mat_env,log = TRUE)
-p1_h0 = dmvnorm(wrong_diff_mat,mean=rep(0,n_pheno),sigma= diag(sqrt(1-r2)) %*% corr_mat_env %*% diag(sqrt(1-r2)),log = TRUE)
-p1_h1 = dmvnorm(diff_mat ,mean=rep(0,n_pheno),sigma= diag(sqrt(1-r2)) %*% corr_mat_env %*% diag(sqrt(1-r2)),log = TRUE)
+quantile_norm = function(x){
+    norm_dist = rnorm(length(x), mean=0, sd=1)
+    ori_order = order(x)
+    sorted_dist = x[ori_order]
+    sorted_new_dist = sort(norm_dist)
+    new_dist = sorted_new_dist[order(ori_order)]
+    return(new_dist)
+}
 
-llr_h1 = p1_h1 - p0
-llr_h0 = p1_h0 - p0
+llr_h1 = compute_llr(X, PRS, r2)
+llr_h0 = compute_llr(X, PRS[c(2:nrow(PRS), 1),], r2)
+
+wrong_r2 = rep(0.5, n_pheno)
+wrong_llr_h1 = compute_llr(X, PRS, wrong_r2)
+wrong_llr_h0 = compute_llr(X, PRS[c(2:nrow(PRS), 1),], wrong_r2)
+
+quant_norm_X = apply(X, 2, quantile_norm)
+quant_norm_PRS = apply(PRS, 2, quantile_norm)
+quant_norm_llr_h1 = compute_llr(quant_norm_X, quant_norm_PRS, r2)
+quant_norm_llr_h0 = compute_llr(quant_norm_X, quant_norm_PRS, r2)
+
+#cat("Try the quantile norm:\n")
+#merged_pgs_df_scaled = apply(merged_pgs_df[,-c('IID')], 2, quantile_norm)
+#merged_pheno_df_scaled = apply(merged_pheno_df[,-c('IID')], 2, quantile_norm)
 
 cat("Plot result:\n")
-plot_df = rbind(data.frame(llr=llr_h1, group="H1"), data.frame(llr=llr_h0, group="H0"))
-p = ggplot(plot_df, aes(llr)) +
+plot_df = rbind(
+	data.frame(llr=llr_h1, group="H1", method="classic"),
+	data.frame(llr=llr_h0, group="H0", method="classic"),
+	data.frame(llr=wrong_llr_h1, group="H1", method="const"),
+	data.frame(llr=wrong_llr_h0, group="H0", method="const"),
+	data.frame(llr=quant_norm_llr_h1, group="H1", method="quant_norm"),
+	data.frame(llr=quant_norm_llr_h0, group="H0", method="quant_norm"))
+p = ggplot(plot_df, aes(x=llr, color=group, linetype=method)) +
 	geom_density()
 
-ggsave()
+ggsave("test.png")
+
 
 
